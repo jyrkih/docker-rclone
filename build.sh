9656183b317294d6f1554924a989e522bba5befe
Jyrki Hokkanen <jhokkan@gmail.com>
2020-07-08 13:52:38 +0300

    build script change

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build --no-cache --build-arg RCLONE_VERSION=v1.52.2 -t jykke/rclone:1.52.2 -t jykke/rclone:latest .
+FILE=Dockerfile
+if [ "$HOSTTYPE" = "arm" ]; then
+  FILE=Dockerfile.armv7
+fi
+docker build --no-cache --build-arg RCLONE_VERSION=v1.52.2 -t jykke/rclone:1.52.2 -t jykke/rclone:latest -f $FILE .
